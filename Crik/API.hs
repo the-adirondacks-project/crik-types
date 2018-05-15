de76aadc413205f416ef96e14374ea5fbb36e906
Jacob Leach <leach.jacob@gmail.com>
2018-05-15 19:57:13 -0400

    Change creating back to requiring no id
    I don't want to give the impression you can select your own id.

--- Crik/API.hs
+++ Crik/API.hs
@@ -52,7 +52,7 @@ type CaptureVideoId = Capture "videoId" Int
 
 type GetVideo = Version :> Videos :> CaptureVideoId :> Get '[JSON] (Video VideoId)
 type GetVideos = Version :> Videos :> Get '[JSON] [Video VideoId]
-type CreateVideo = Version :> Videos :> ReqBody '[JSON] (Video (Maybe VideoId)) :> Post '[JSON] (Video VideoId)
+type CreateVideo = Version :> Videos :> ReqBody '[JSON] (Video NoId) :> Post '[JSON] (Video VideoId)
 type UpdateVideo = Version :> Videos :> CaptureVideoId :> ReqBody '[JSON] (Video (Maybe VideoId))
   :> Put '[JSON] (Video VideoId)
 type GetFilesForVideo = Version :> Videos :> CaptureVideoId :> Get '[JSON] [VideoFile VideoFileId]
