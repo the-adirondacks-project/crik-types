2d8fe4546001fd9b50d0588aa53f0a7b873ee6e2
Jacob Leach <leach.jacob@gmail.com>
2018-06-23 15:52:51 -0400

    Add some types from the CLI
    Not sure if they really belong here but its easy.

--- Crik/Types/Library.hs
+++ Crik/Types/Library.hs
@@ -6,26 +6,47 @@
 
 module Crik.Types.Library
 (
-  LibraryId(..)
-, Library(..)
+  Library(..)
+, LibraryId(..)
+, LibraryType(..)
+, libraryType
 ) where
 
 import Data.Aeson (FromJSON(parseJSON), Value(Object), ToJSON(toJSON, toEncoding), (.=), (.:), object, pairs)
 import Data.Aeson.Types (typeMismatch)
 import Data.Semigroup (Semigroup ((<>)))
-import Data.Text (Text)
+import Data.String (IsString(..))
+import Data.Text (Text, isPrefixOf)
 import GHC.Generics (Generic)
 
 import Crik.TH.DeriveHttpData
 import Crik.Types.Internal (NoId(NoId))
 import Crik.Types.Library.Id
+
 import Crik.Types.Video (VideoId)
+
+-- TODO: Does this belong? Should this just be stored?
+data LibraryType =
+  HTTP |
+  Directory
+  deriving (Show)
+
+instance IsString LibraryType where
+  fromString text = case text of
+    "http" -> HTTP
+    "directory" -> Directory
 
 data Library id = Library {
   libraryId :: id
 , libraryUrl :: Text
 , libraryName :: Text
 } deriving (Generic, Show)
+
+libraryType :: Library a -> LibraryType
+libraryType Library{..}
+  | "file://" `isPrefixOf` libraryUrl = Directory
+  | "http://" `isPrefixOf` libraryUrl = HTTP
+  | otherwise = undefined -- TODO: Handle this properly
 
 -- To JSON instances
 instance ToJSON (Library NoId) where
